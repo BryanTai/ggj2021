53ef32b5ec52ed53e2e0d401d63da4dfdffd0a71
Bryan Tai <redhatpieman@gmail.com>
2021-01-28 22:40:50 -0800

    Added 3rd person Camera and movement

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Followed Brackeys tutorial on THIRD PERSON MOVEMENT
+// https://www.youtube.com/watch?v=4HpC--2iowE
+public class ThirdPersonMovement : MonoBehaviour
+{
+	public CharacterController controller;
+	public Transform camera;
+
+	public float Speed = 6f;
+	public float TurnSmoothTime = 0.1f;
+	float turnSmoothVelocity;
+
+	private void Update()
+	{
+		float horizontal = Input.GetAxisRaw("Horizontal"); //between -1 and 1, (A key, D key)
+		float vertical = Input.GetAxisRaw("Vertical"); //between -1 and 1, (W key, S key)
+
+		Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+		if(direction.magnitude >= 0.1f)
+		{
+			float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+			float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, TurnSmoothTime);
+			transform.rotation = Quaternion.Euler(0f, smoothedAngle, 0f);
+
+			Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+			controller.Move(moveDirection * Speed * Time.deltaTime);
+		}
+	}
+}
