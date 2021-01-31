f3d51abfd117c05ffa928bdaa4b6f0099a15a94b
Qiao <Qiao@Mac-Pro.local>
2021-01-30 19:25:27 -0800

    added sfx file, sfx trigger script, and modified player and magazine prefab

---
+++ Assets/Scripts/TriggerSFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSFX : MonoBehaviour
+{
+    public AudioSource playSound;
+    private ItemPickup _currentItem = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //print("other name = " + other.gameObject.name);
+        _currentItem = other.gameObject.GetComponent<ItemPickup>();
+
+        if(_currentItem == null)
+        {
+            return;
+            
+        }
+        playSound.Play();
+        // print("GameObject name = " + this.gameObject.name);
+
+        
+    }
+}
