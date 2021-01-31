9e082e7a12bdb8585baf8031be0044686a5dc294
Qiao <Qiao@Mac-Pro.local>
2021-01-30 22:07:33 -0800

    player grow shrink script

---
+++ Assets/Scripts/PlayerSize.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSize : MonoBehaviour
+{
+    Inventory inventory;
+    int currentItemNum = 0;
+    private Vector3 LargeScale = new Vector3(3.0f, 3.0f, 3.0f);
+    private Vector3 MediumScale = new Vector3(2.0f, 2.0f, 2.0f);
+    private Vector3 SmallScale = new Vector3 (1.0f, 1.0f, 1.0f); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = SmallScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentItemNum = Inventory.instance.items.Count;
+        print(currentItemNum);
+
+        if(currentItemNum <= 5)
+        {
+            transform.localScale = SmallScale;
+        }
+        else if(currentItemNum > 5  && currentItemNum <= 10)
+        {
+            transform.localScale = MediumScale;
+        }
+        else if(currentItemNum > 10)
+        {
+            transform.localScale = LargeScale;
+        }
+    }
+}
